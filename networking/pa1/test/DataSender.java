1b228dc44753cb169c8ea95778990ffa552bca19
jandrepont <joelandrepont1@gmail.com>
2018-02-20 11:56:21 -0600

    DataSender now sends packets whenever a client connects to it

--- networking/pa1/test/DataSender.java
+++ networking/pa1/test/DataSender.java
@@ -1,58 +1,24 @@
-/*
- * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-import java.io.*;
 import java.net.*;
+import java.net.Socket;
+import java.io.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.*;
 
-
 public class DataSender {
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 4) {
-            System.err.println(
-                    "Usage: java EchoClient <host name> <port number> <interval sec> <num of different packets/sec>");
+        if (args.length != 3) {
+            System.err.println("Usage: java DataReceiver <port number> <interval sec> <num of different packets/sec>");
             System.exit(1);
         }
-
         int[] array = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         int i, n, rate;
         ArrayList<Integer> frequency = new ArrayList<Integer>();
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-        int interval = Integer.parseInt(args[2]);
+        int portNumber = Integer.parseInt(args[0]);
+        int interval = Integer.parseInt(args[1]);
         interval = interval*1000; //sec to ms
-        int n_frequencys = Integer.parseInt(args[3]);
+        int n_frequencys = Integer.parseInt(args[2]);
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         for(i = 0; i < n_frequencys; i++ ){
             System.out.printf("How many packets per sec for frequency[%d]", i);
@@ -61,22 +27,18 @@ public class DataSender {
         }
         reader.close();
 
-
         try (
-                Socket echoSocket = new Socket(hostName, portNumber);
+                ServerSocket serverSocket =
+                        new ServerSocket(Integer.parseInt(args[0]));
+                Socket clientSocket = serverSocket.accept();
                 PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
-                DataOutputStream output = new DataOutputStream(echoSocket.getOutputStream());
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
+                new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+                DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
         ) {
-
-            String userInput;
-            PrintStream packet;
+            System.out.println("Socket = " + clientSocket.getRemoteSocketAddress());
+            String inputLine;
             long end;
             while (true) {
                 for(i = 0; i < n_frequencys; i++){
@@ -88,13 +50,10 @@ public class DataSender {
                     }
                 }
             }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
         } catch (InterruptedException e){
             e.printStackTrace();
         }
