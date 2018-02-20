3aee0e1b88525002a575e315e391a0de60897853
jandrepont <joelandrepont1@gmail.com>
2018-02-20 11:58:14 -0600

    DataSender now sends packets whenever a client connects to it, removed dir test

--- networking/pa1/DataSender.java
+++ networking/pa1/DataSender.java
@@ -1,41 +1,61 @@
-import java.io.*;
 import java.net.*;
+import java.net.Socket;
+import java.io.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.*;
 
 public class DataSender {
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 2) {
-            System.err.println(
-                    "Usage: java EchoClient <host name> <port number>");
+        if (args.length != 3) {
+            System.err.println("Usage: java DataReceiver <port number> <interval sec> <num of different packets/sec>");
             System.exit(1);
         }
-
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
+        int[] array = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int i, n, rate;
+        ArrayList<Integer> frequency = new ArrayList<Integer>();
+        int portNumber = Integer.parseInt(args[0]);
+        int interval = Integer.parseInt(args[1]);
+        interval = interval*1000; //sec to ms
+        int n_frequencys = Integer.parseInt(args[2]);
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        for(i = 0; i < n_frequencys; i++ ){
+            System.out.printf("How many packets per sec for frequency[%d]", i);
+            n = reader.nextInt();
+            frequency.add(n);
+        }
+        reader.close();
 
         try (
-                Socket echoSocket = new Socket(hostName, portNumber);
+                ServerSocket serverSocket =
+                        new ServerSocket(Integer.parseInt(args[0]));
+                Socket clientSocket = serverSocket.accept();
                 PrintWriter out =
-                        new PrintWriter(echoSocket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(echoSocket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
+                new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+                DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
         ) {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo: " + in.readLine());
+            System.out.println("Socket = " + clientSocket.getRemoteSocketAddress());
+            String inputLine;
+            long end;
+            while (true) {
+                for(i = 0; i < n_frequencys; i++){
+                    rate = interval/frequency.get(i);
+                    end = System.currentTimeMillis() + interval;
+                    while(System.currentTimeMillis() < end){
+                        output.writeBytes(Arrays.toString(array) + "frequency = " + Integer.toString(rate) + '\n');
+                        Thread.sleep((rate/2));
+                    }
+                }
             }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e){
+            e.printStackTrace();
         }
     }
 }
